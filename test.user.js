9f20e708becc37eb4eb0f0e47d9ad0ceb47525fd
julzhk <jharley@vam.ac.uk>
2013-06-05 11:02:16 +0100

    use jquery to add a pop up showing no links on a page

--- test.user.js
+++ test.user.js
@@ -1,7 +1,30 @@
-/**
- * Created with PyCharm.
- * User: julz
- * Date: 05/06/2013
- * Time: 10:47
- * To change this template use File | Settings | File Templates.
- */
+// ==UserScript==
+// @name       Testing Userscripts
+// @namespace  http://use.i.E.your.homepage/
+// @version    0.1
+// @description  Pop up a form dialogue
+//  @match      http://*/*
+// @copyright  NA
+// ==/UserScript==
+function addJQuery(callback) {
+  var script = document.createElement("script");
+  script.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js");
+  script.addEventListener('load', function() {
+    var script = document.createElement("script");
+    script.textContent = "(" + callback.toString() + ")();";
+    document.body.appendChild(script);
+  }, false);
+  document.body.appendChild(script);
+}
+
+// the guts of this userscript
+function main() {
+  alert("There are " + $('a').length + " links on this page.");
+}
+
+// load jQuery and execute the main function
+addJQuery(main);
+
+
+alert('hi world');
+$( "#dialog-form" ).dialog( "open" );
