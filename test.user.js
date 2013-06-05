ef2cda443325cdbff41ede9cf84aaf044692ae3a
julzhk <jharley@vam.ac.uk>
2013-06-05 17:40:55 +0100

    better versions for j query

--- test.user.js
+++ test.user.js
@@ -6,20 +6,52 @@
 //  @match      http://*/*
 // @copyright  NA
 // ==/UserScript==
-function addJQuery(callback) {
-  var script = document.createElement("script");
-  script.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js");
-  script.addEventListener('load', function() {
-    var script = document.createElement("script");
-    script.textContent = "(" + callback.toString() + ")();";
-    document.body.appendChild(script);
-  }, false);
-  document.body.appendChild(script);
-}
 
+
+function addJQuery(callback) {
+
+  var script = document.createElement("script");
+  var script2 = document.createElement("script");
+  var script3 = document.createElement("link");
+  script.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js");
+  script2.setAttribute("src", "//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js");
+  script3.setAttribute("href", "//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css");   
+  script3.setAttribute("rel", "stylesheet");   
+  script.addEventListener('load', function() {
+      console.log('21');
+  var script = document.createElement("script");
+  script.textContent = "(" + callback.toString() + ")();";
+  document.body.appendChild(script);   
+    document.body.appendChild(script2);
+    document.body.appendChild(script3);
+  }, false);
+    document.body.appendChild(script);
+    document.body.appendChild(script2);
+    document.body.appendChild(script3);
+
+ 
+}
+ 
 // the guts of this userscript
 function main() {
-  alert("There are " + $('a').length + " links on this page.");
+//  alert("There are " + $('a').length + " links on this page.");
+    var modalcontent = '<a href="#myModal" role="button" class="btn" data-toggle="modal">Launch demo modal</a>'+
+    '<div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">'+
+    '<div class="modal-header">'+
+    '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>'+
+    '<h3 id="myModalLabel">Modal header</h3>'+
+    '</div>'+
+    '<div class="modal-body">'+
+    ' <p>One fine body</p>'+
+    '</div>'+
+    '<div class="modal-footer">'+
+    '<button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>'+
+    '<button class="btn btn-primary">Save changes</button>'+
+    '</div>'+
+'</div>';
+
+//    $('h1').append('woow!');
+	$('div').append(modalcontent);    
 }
 
 // load jQuery and execute the main function
